5cd99caa9e7e36f82fcce48deb647373a25fc63c
Volkov Daniil <volkov.study@mail.ru>
2022-11-13 17:46:21 +0300

    Create Client class

---
+++ public-entities/client/Client.cpp
@@ -0,0 +1,44 @@
+#include "Client.h"
+
+#include <utility>
+
+int CLIENT_AMOUNT;
+
+Client::Client(string fullName, string address, string email) :
+                                            fullName(std::move(fullName)),
+                                            address(std::move(address)),
+                                            email(std::move(email)) {
+    Client::id = ++CLIENT_AMOUNT;
+}
+
+int Client::getId() const {
+    return id;
+}
+
+void Client::setId(int id) {
+    Client::id = id;
+}
+
+const string &Client::getFullName() const {
+    return fullName;
+}
+
+void Client::setFullName(const string &fullName) {
+    Client::fullName = fullName;
+}
+
+const string &Client::getAddress() const {
+    return address;
+}
+
+void Client::setAddress(const string &address) {
+    Client::address = address;
+}
+
+const string &Client::getEmail() const {
+    return email;
+}
+
+void Client::setEmail(const string &email) {
+    Client::email = email;
+}
---
+++ public-entities/client/Client.h
@@ -0,0 +1,31 @@
+#ifndef MARKETPLACE_ERP_TUI_CLIENT_H
+#define MARKETPLACE_ERP_TUI_CLIENT_H
+
+#include <string>
+
+using namespace std;
+
+class Client {
+private:
+    int id;
+    string fullName;
+    string address;
+    string email;
+public:
+    Client(string fullName, string address, string email);
+
+    int getId() const;
+    void setId(int id);
+
+    const string &getFullName() const;
+    void setFullName(const string &fullName);
+
+    const string &getAddress() const;
+    void setAddress(const string &address);
+
+    const string &getEmail() const;
+    void setEmail(const string &email);
+};
+
+
+#endif

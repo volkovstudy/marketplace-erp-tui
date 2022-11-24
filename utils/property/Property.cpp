352c4f9d4321a8e7edde4b21c2c78d781ddd1638
Volkov Daniil <volkov.study@mail.ru>
2022-11-24 19:51:50 +0300

    Create new class: Property

---
+++ utils/property/Property.cpp
@@ -0,0 +1,23 @@
+#include "Property.h"
+
+#include <utility>
+
+Property::Property(string fieldName, const vector<string> &values) :
+                                            fieldName(std::move(fieldName)),
+                                            values(values) {}
+
+const string &Property::getFieldName() const {
+    return fieldName;
+}
+
+void Property::setFieldName(const string &newFieldName) {
+    Property::fieldName = newFieldName;
+}
+
+const vector<string> &Property::getValues() const {
+    return values;
+}
+
+void Property::setValues(const vector<string> &newValues) {
+    Property::values = newValues;
+}
---
+++ utils/property/Property.h
@@ -0,0 +1,24 @@
+#ifndef MARKETPLACE_ERP_TUI_PROPERTY_H
+#define MARKETPLACE_ERP_TUI_PROPERTY_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Property {
+private:
+    string fieldName;
+    vector<string> values;
+public:
+    Property(string fieldName, const vector<string> &values);
+
+    const string &getFieldName() const;
+    void setFieldName(const string &fieldName);
+
+    const vector<string> &getValues() const;
+    void setValues(const vector<string> &values);
+};
+
+
+#endif

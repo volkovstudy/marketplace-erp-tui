3877bbb507bbd3edd39b7d1fe9363d92fe6a5a41
Volkov Daniil <volkov.study@mail.ru>
2022-11-24 19:54:14 +0300

    Create new class: CsvFileService

---
+++ utils/csvfileservice/CsvFileService.cpp
@@ -0,0 +1,80 @@
+#include "CsvFileService.h"
+
+#include <utility>
+#include <fstream>
+#include <vector>
+
+#define CSV_DELIMITER ","
+
+using namespace std;
+
+CsvFileService::CsvFileService(string filePath) : filePath(std::move(filePath)) {}
+
+const string &CsvFileService::getFilePath() const {
+    return filePath;
+}
+
+void CsvFileService::setFilePath(const string &newFilePath) {
+    CsvFileService::filePath = newFilePath;
+}
+
+vector<string> splitLine(const string& str, const string& delimiter) {
+    vector<string> result;
+
+    string::size_type currentPosition = str.find(delimiter);;
+    string::size_type previousElementIndex = 0;
+
+    while (currentPosition != string::npos) {
+        string delimitedLine = str.substr(previousElementIndex, currentPosition - previousElementIndex);
+        result.push_back(delimitedLine);
+
+        previousElementIndex = currentPosition + delimiter.size();
+        currentPosition = str.find(delimiter, previousElementIndex);
+    }
+
+    string lastDelimitedLine = str.substr(previousElementIndex);
+    result.push_back(lastDelimitedLine);
+
+    return result;
+}
+
+vector<Property*> createVectorWithProperties(vector<string> fieldNames) {
+    vector<Property*> result;
+
+    for (int i = 0; i < fieldNames.size(); ++i) {
+        string fieldName = fieldNames.at(i);
+        Property *property = new Property(fieldName, {});
+
+        result.push_back(property);
+    }
+
+    return result;
+}
+
+vector<Property*> CsvFileService::getAllLines() {
+    ifstream fileReader(filePath);
+
+    string lineWithColumnNames;
+    getline(fileReader, lineWithColumnNames);
+
+    vector<string> fieldNames = splitLine(lineWithColumnNames, CSV_DELIMITER);
+    vector<Property*> allLines = createVectorWithProperties(fieldNames);
+
+    string line;
+    while (getline(fileReader, line)) {
+        vector<string> lineValues = splitLine(line, CSV_DELIMITER);
+
+        for (int i = 0; i < allLines.size(); ++i) {
+            Property *property = allLines.at(i);
+
+            vector<string> values = property->getValues();
+            values.push_back(lineValues.at(i));
+
+            property->setValues(values);
+        }
+    }
+
+    fileReader.close();
+
+    return allLines;
+}
---
+++ utils/csvfileservice/CsvFileService.h
@@ -0,0 +1,22 @@
+#ifndef MARKETPLACE_ERP_TUI_CSVFILESERVICE_H
+#define MARKETPLACE_ERP_TUI_CSVFILESERVICE_H
+
+#include <string>
+#include "../property/Property.h"
+
+using namespace std;
+
+class CsvFileService {
+private:
+    string filePath;
+public:
+    explicit CsvFileService(string filePath);
+
+    vector<Property*> getAllLines();
+
+    const string &getFilePath() const;
+    void setFilePath(const string &newFilePath);
+};
+
+
+#endif

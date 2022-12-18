42351358a7e1dd42df20162b638723999a79b74b
Volkov Daniil <volkov.study@mail.ru>
2022-12-18 19:51:09 +0300

    Create new class: TablePrinter without methods realizations

---
+++ table-printer/TablePrinter.cpp
@@ -0,0 +1,29 @@
+#include "TablePrinter.h"
+
+#include <iostream>
+
+#define indentLength 10
+
+using namespace std;
+
+void printHorizontalLine(int length);
+
+void printHeaderWithNameOfTable(const string& tableName, int lineLength);
+
+void printHorizontalLine(int length) {
+    for (int i = 0; i < length; ++i) {
+        cout << "-";
+    }
+    cout << endl;
+}
+
+void printHeaderWithNameOfTable(const string& tableName, int lineLength) {
+    int lengthOfIndent = lineLength / 2;
+
+    cout << "|";
+    cout.width(lengthOfIndent);
+    cout << tableName;
+    cout.width(lengthOfIndent - 1); // -1 => because begins printing after all width
+    cout << "|";
+    cout << endl;
+}
---
+++ table-printer/TablePrinter.h
@@ -0,0 +1,14 @@
+#ifndef MARKETPLACE_ERP_TUI_TESTS_TABLEPRINTER_H
+#define MARKETPLACE_ERP_TUI_TESTS_TABLEPRINTER_H
+
+
+#include <vector>
+#include "../models/client/Client.h"
+
+class TablePrinter {
+public:
+    static void printVector(const vector<Client*>& clients);
+};
+
+
+#endif

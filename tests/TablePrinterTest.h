18270a211a532a71155f9a430e70ff2cd95d9c1c
Volkov Daniil <volkov.study@mail.ru>
2022-12-20 14:26:54 +0300

    Create test for TablePrinter#printVector(...Order...)

--- tests/TablePrinterTest.h
+++ tests/TablePrinterTest.h
@@ -11,6 +11,12 @@ public:
         TablePrinter::printVector(clients);
     }
 
+    static void shouldPrintVectorOfOrders() {
+        vector<Order*> orders = givenFourOrders();
+
+        TablePrinter::printVector(orders);
+    }
+
 private:
     static vector<Client*> givenFourClients() {
         vector<Client*> clients;
@@ -21,6 +27,31 @@ private:
         clients.push_back(new Client("Petr", "petya@example.com"));
 
         return clients;
+    }
+
+    static vector<Order*> givenFourOrders() {
+        vector<Order*> orders;
+        vector<Client*> clients = givenFourClients();
+
+        map<string, int> products1;
+        products1["tomato"] = 5;
+        products1["cucumbers"] = 2;
+        orders.push_back(new Order(1, clients.at(0), products1));
+
+        map<string, int> products2;
+        products2["tv"] = 1;
+        products2["playstation"] = 1;
+        orders.push_back(new Order(2, clients.at(1), products2));
+
+        map<string, int> products3;
+        products3["table"] = 1;
+        orders.push_back(new Order(3, clients.at(2), products3));
+
+        map<string, int> products4;
+        products4["Long product"] = 1000;
+        orders.push_back(new Order(4, clients.at(3), products4));
+
+        return orders;
     }
 };
 

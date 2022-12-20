c1dc2ef50ce81f3282f69cb530a24d8b519f65d1
Volkov Daniil <volkov.study@mail.ru>
2022-12-20 13:23:16 +0300

    Realize TablePrinter#printVector(...) for Client class

--- table-printer/TablePrinter.cpp
+++ table-printer/TablePrinter.cpp
@@ -12,6 +12,48 @@ void printHeader(int lineLength, const string& tableName);
 
 void printTableName(const string& tableName, int lineLength);
 
+void TablePrinter::printVector(const vector<Client*>& clients) {
+    int maxLengthOfName = 0, maxLengthOfEmail = 0;
+    for (Client* client: clients) {
+        int nameLength = (int) client->getFullName().length();
+        maxLengthOfName = max(maxLengthOfName, nameLength);
+
+        int emailLength = (int) client->getEmail().length();
+        maxLengthOfEmail = max(maxLengthOfEmail, emailLength);
+    }
+
+    // indentLength * 2 => because of left & right indent
+    // + 3 => because of three '|' characters in header
+    int lineLength = maxLengthOfName + maxLengthOfEmail + (indentLength * 2) + 3;
+    printHeader(lineLength, "*Clients*");
+
+    cout << "|";
+    cout.width(maxLengthOfName + indentLength);
+    cout << "NAME " << " |";
+
+    cout.width(maxLengthOfEmail + indentLength - 2); // -2 => because of two spaces in next line
+    cout << "EMAIL " << " |";
+    cout << endl;
+
+    printHorizontalLine(lineLength);
+
+    for (Client* client: clients) {
+        cout << "| ";
+        cout.width(maxLengthOfName + indentLength - 1);
+        cout << right;
+        cout << client->getFullName();
+        cout << " |";
+
+        cout.width(maxLengthOfEmail + indentLength -
+                   2); // -2 => because of one space in " |" in NAME column & one space in the end
+        cout << right;
+        cout << client->getEmail();
+        cout << " |" << endl;
+    }
+
+    printHorizontalLine(lineLength);
+}
+
 void printHorizontalLine(int length) {
     for (int i = 0; i < length; ++i) {
         cout << "-";

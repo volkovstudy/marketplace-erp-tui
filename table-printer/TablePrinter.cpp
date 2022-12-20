a6b7141ffc9b018c8e564fe66868fe628c79814a
Volkov Daniil <volkov.study@mail.ru>
2022-12-20 15:11:02 +0300

    Implemented TablePrinter#printVector(...Order...)

--- table-printer/TablePrinter.cpp
+++ table-printer/TablePrinter.cpp
@@ -1,6 +1,7 @@
 #include "TablePrinter.h"
 
 #include <iostream>
+#include <sstream>
 
 #define indentLength 10
 
@@ -52,6 +53,103 @@ void TablePrinter::printVector(const vector<Client*>& clients) {
     }
 
     printHorizontalLine(lineLength);
+}
+
+string makeClientLine(Client& client) {
+    stringstream clientLine;
+    clientLine << client.getFullName() << " <" << client.getEmail() << ">";
+    return clientLine.str();
+}
+
+string makeProductLine(const pair<string, int>& product) {
+    stringstream productLine;
+    productLine << product.first << " - " << product.second << " units";
+    return productLine.str();
+}
+
+void TablePrinter::printVector(const vector<Order*>& orders) {
+    map<Order*, string> idStrings;
+    map<Order*, string> clientLines;
+    map<Order*, vector<string>> productLines;
+
+    int maxLengthOfId = 0, maxLengthOfClientLine = 0, maxLengthOfProductLine = 0;
+    for (Order* order: orders) {
+        string idString = to_string(order->getId());
+        idStrings[order] = idString;
+        int idLength = (int) idString.length();
+        maxLengthOfId = max(maxLengthOfId, idLength);
+
+        Client client = *(order->getClient());
+        string clientLine = makeClientLine(client);
+        clientLines[order] = clientLine;
+        maxLengthOfClientLine = max(maxLengthOfClientLine, (int) clientLine.length());
+
+        vector<string> productsLines;
+        for (pair<string, int> product: order->getProducts()) {
+            string productLine = makeProductLine(product);
+            maxLengthOfProductLine = max(maxLengthOfProductLine, (int) productLine.length());
+
+            productsLines.push_back(productLine);
+        }
+        productLines[order] = productsLines;
+    }
+
+    // indentLength * 2 => because of left & right indent
+    // + 4 => because of four '|' characters in header
+    // + 4 => because of spaces before '|' characters in header
+    int lineLength = maxLengthOfId + maxLengthOfClientLine + maxLengthOfProductLine + (indentLength * 2) + 4 + 4;
+    printHeader(lineLength, "*Orders*");
+
+    cout << "|";
+    cout.width(maxLengthOfId + indentLength);
+    cout << "ID " << " |";
+
+    cout.width(maxLengthOfClientLine + 1); // +1 => because of 1 space in " |"
+    cout << "CLIENT " << " |";
+
+    cout.width(maxLengthOfProductLine + indentLength);
+    cout << "PRODUCTS " << " |";
+    cout << endl;
+
+    printHorizontalLine(lineLength);
+
+    int widthForId = maxLengthOfId + indentLength;
+    // +1 => because we need a space after previous '|' in ID column AND because we don't have indent there
+    int widthForClient = maxLengthOfClientLine + 1;
+    int widthForProducts = maxLengthOfProductLine + indentLength;
+
+    for (Order* order: orders) {
+        cout << "|";
+
+        cout.width(widthForId);
+        cout << right << idStrings[order] << " |";
+
+        cout.width(widthForClient);
+        cout << right << clientLines[order] << " |";
+
+        vector<string> productsLines = productLines[order];
+        cout.width(widthForProducts);
+        cout << right << (productsLines.empty() ? "" : productsLines.at(0)) << " |" << endl;
+
+        // Starts from 1 because we've already printed the first line
+        for (int i = 1; i < productsLines.size(); ++i) {
+            string productLine = productsLines.at(i);
+
+            cout << "|";
+
+            cout.width(widthForId + 2); // +2 => because of the need to move the '|' to the right
+            cout << right << "|";
+
+            cout.width(widthForClient + 2); // +2 => because of the need to move the '|' to the right
+            cout << right << "|";
+
+            cout.width(widthForProducts);
+            cout << right;
+            cout << productLine << " |" << endl;
+        }
+
+        printHorizontalLine(lineLength);
+    }
 }
 
 void printHorizontalLine(int length) {

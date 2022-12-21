bea0d026760a5277fdc4b4da46a633f6e025444d
Volkov Daniil <volkov.study@mail.ru>
2022-12-21 12:34:11 +0300

    Create ProgramLauncher.cpp#toLowerCase(...)

--- src/program-launcher/ProgramLauncher.cpp
+++ src/program-launcher/ProgramLauncher.cpp
@@ -1,1 +1,14 @@
 #include "ProgramLauncher.h"
+#include <algorithm>
+#include <cctype>
+#include <string>
+
+using namespace std;
+
+void toLowerCase(string* line);
+
+void toLowerCase(string* line) {
+    transform(line->begin(), line->end(), line->begin(),
+              [](unsigned char c) { return tolower(c); }
+    );
+}

f8719972e6237cc65037486cf36b977cf5ba26dc
Volkov Daniil <volkov.study@mail.ru>
2022-12-21 15:38:33 +0300

    Implement ClientDialog::addNewClient()

--- src/program-launcher/dialogues/client-dialog/ClientDialog.cpp
+++ src/program-launcher/dialogues/client-dialog/ClientDialog.cpp
@@ -3,8 +3,10 @@
 #include <iostream>
 
 #include "../../../utils/Utils.h"
+#include "../../../repositories/client-repository/ClientRepository.h"
 
 #define tab "\t"
+#define pathToClientsFile "marketplace-erp/clients.csv"
 
 #define addNewClientProgramChar "a"
 #define listAllClientsProgramChar "l"
@@ -16,8 +18,11 @@
 
 using namespace std;
 
+void getInformationAboutClient(string& name, string& email);
+void saveClient(Client client);
+
 void ClientDialog::openManagementDialog() {
-    cout << "You opened clients management section.";
+    cout << "You opened clients management section. ";
     printSectionVariants();
 
     do {
@@ -47,6 +52,7 @@ bool ClientDialog::getChoiceAndExecuteActionAndReturnQuitResult() {
         Utils::toLowerCase(&program);
 
         if (program == addNewClientProgramChar) {
+            addNewClient();
             break;
         } else if (program == listAllClientsProgramChar) {
             break;
@@ -59,4 +65,56 @@ bool ClientDialog::getChoiceAndExecuteActionAndReturnQuitResult() {
     } while (true);
 
     return false;
+}
+
+void ClientDialog::addNewClient() {
+    cout << "Now you're going to add new client." << endl;
+
+    string name, email;
+    getInformationAboutClient(name, email);
+
+    do {
+        cout << endl << "Well, check the information about new client:" << endl;
+        cout << "Name" << tab << name << endl;
+        cout << "Email" << tab << email << endl;
+
+        cout << endl << "Is it right?" << endl;
+        cout << "(Y)es, (E)dit, (C)ancel: ";
+
+        string program;
+        cin >> program;
+        Utils::toLowerCase(&program);
+
+        if (program == yesChar) {
+            Client client(name, email);
+            saveClient(client);
+
+            break;
+        } else if (program == editChar) {
+            getInformationAboutClient(name, email);
+        } else if (program == cancelChar) {
+            cout << endl << "You left adding new client." << endl;
+            return;
+        } else {
+            cout << endl << "Wrong input. Try again." << endl;
+        }
+    } while (true);
+}
+
+void getInformationAboutClient(string& name, string& email) {
+    cout << "Write the name: ";
+    do {
+        getline(cin, name);
+    } while (name.empty());
+
+    cout << "Write the email: ";
+    do {
+        cin >> email;
+    } while (email.empty());
+}
+
+void saveClient(Client client) {
+    ClientRepository clientRepository(pathToClientsFile);
+
+    clientRepository.save(&client);
 }

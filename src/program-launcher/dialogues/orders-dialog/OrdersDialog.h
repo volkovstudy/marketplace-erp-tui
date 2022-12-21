a347aaea2602d905ad39ce6bca7c7ae27a9d5c33
Volkov Daniil <volkov.study@mail.ru>
2022-12-21 14:21:35 +0300

    Create new class without realization: OrdersDialog

---
+++ src/program-launcher/dialogues/orders-dialog/OrdersDialog.h
@@ -0,0 +1,13 @@
+#ifndef MARKETPLACE_ERP_TUI_ORDERSDIALOG_H
+#define MARKETPLACE_ERP_TUI_ORDERSDIALOG_H
+
+
+class OrdersDialog {
+public:
+    static void openManagementDialog();
+    static void addNewOrder();
+    static void listAllOrders();
+};
+
+
+#endif

3c6c00cdb953ceba3bf39c960091ae032bbf185d
Volkov Daniil <volkov.study@mail.ru>
2022-12-21 14:01:26 +0300

    Create new static method: Utils::toLowerCase(...)

--- src/utils/Utils.cpp
+++ src/utils/Utils.cpp
@@ -1,1 +1,9 @@
 #include "Utils.h"
+
+#include <algorithm>
+
+void Utils::toLowerCase(string* line) {
+    transform(line->begin(), line->end(), line->begin(),
+              [](unsigned char c) { return tolower(c); }
+    );
+}
--- src/utils/Utils.h
+++ src/utils/Utils.h
@@ -2,7 +2,13 @@
 #define MARKETPLACE_ERP_TUI_UTILS_H
 
 
+#include <string>
+
+using namespace std;
+
 class Utils {
+public:
+    static void toLowerCase(string* line);
 };
 
 

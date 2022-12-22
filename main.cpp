52acf789d8e1f5ccc837c6ad387284472e98410e
Volkov Daniil <volkov.study@mail.ru>
2022-12-22 17:36:22 +0300

    Add launching program in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+#include <dirent.h>
+#include "src/program-launcher/ProgramLauncher.h"
+#include "src/file-services/configuration-service/ConfigurationService.h"
+
+#define mainDirectory "marketplace-erp"
 
 using namespace std;
 
+void createDirectoryIfDoesNotExist(const string& name);
+
 int main() {
+    createDirectoryIfDoesNotExist(mainDirectory);
+
+    string configFilePath = mainDirectory"/config.csv";
+    ConfigurationService::setMainConfigFilePath(configFilePath);
+
+    ProgramLauncher::start();
+
     return 0;
+}
+
+void createDirectoryIfDoesNotExist(const string& name) {
+    DIR* dir = opendir(name.c_str());
+
+    string command = "mkdir ";
+    command += name;
+
+    if (!dir) {
+        ::system(command.c_str());
+    }
 }

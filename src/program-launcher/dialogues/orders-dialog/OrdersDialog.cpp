91b3846d9574669f2a7b3f5d13ba76dc763f7945
Volkov Daniil <volkov.study@mail.ru>
2022-12-22 19:46:32 +0300

    Use OrderAddingDialog in OrdersDialog::addNewOrder()

--- src/program-launcher/dialogues/orders-dialog/OrdersDialog.cpp
+++ src/program-launcher/dialogues/orders-dialog/OrdersDialog.cpp
@@ -1,15 +1,12 @@
 #include "OrdersDialog.h"
 #include "../../../utils/Utils.h"
-#include "../../../models/client/Client.h"
-#include "../../../models/order/Order.h"
 #include "../../../repositories/client-repository/ClientRepository.h"
 #include "../../../repositories/order-repository/OrderRepository.h"
 #include "../../../table-printer/TablePrinter.h"
+#include "order-adding-dialog/OrderAddingDialog.h"
 
 #include <iostream>
-#include <sstream>
 
-#define tab "\t"
 #define pathToClientsFile "marketplace-erp/clients.csv"
 #define pathToOrdersFile "marketplace-erp/orders.csv"
 #define pathToProductsFile "marketplace-erp/products.csv"
@@ -18,20 +15,7 @@
 #define listAllOrdersProgramChar "l"
 #define quitProgramChar "q"
 
-#define yesChar "y"
-#define noChar "n"
-#define editChar "e"
-#define cancelChar "c"
-
 using namespace std;
-
-void getInformationAboutOrder(Client& client, map<string, int>& products);
-vector<Client*> getAllClients();
-string makeClientLine(const Client& client);
-void printAllClients();
-void readAndSaveProduct(map<string, int>& products);
-void printProducts(map<string, int>& products);
-void saveOrder(Order& order);
 
 void OrdersDialog::openManagementDialog() {
     cout << "You opened orders management section. ";
@@ -88,189 +72,7 @@ bool OrdersDialog::getChoiceAndExecuteActionAndReturnQuitResult() {
 }
 
 void OrdersDialog::addNewOrder() {
-    cout << "Now you're going to add new order." << endl << endl;
-
-    Client& client = *new Client("", "");
-    map<string, int> products{};
-
-
-    try {
-        getInformationAboutOrder(client, products);
-    } catch (exception& exception) {
-        cout << "There are no clients! Add new client!" << endl;
-        return;
-    }
-
-    do {
-        Utils::printDelimiter();
-
-        cout << "Well, check the information about new order:" << endl << endl;
-        cout << "Client:" << endl << makeClientLine(client) << endl << endl;
-        cout << "Products:" << endl;
-        printProducts(products);
-
-        cout << endl << "Is it right?" << endl;
-        cout << "(Y)es, (E)dit, (C)ancel: ";
-
-        string program;
-        cin >> program;
-        Utils::toLowerCase(&program);
-
-        if (program == yesChar) {
-            Order order(&client, products);
-            saveOrder(order);
-            break;
-        } else if (program == editChar) {
-            Utils::printDelimiter();
-
-            products = {};
-
-            getInformationAboutOrder(client, products);
-        } else if (program == cancelChar) {
-            cout << endl << "You left adding new order." << endl;
-            return;
-        } else {
-            cout << endl << "Wrong input. Try again." << endl;
-        }
-    } while (true);
-}
-
-void getInformationAboutOrder(Client& client, map<string, int>& products) {
-    vector<Client*> clients = getAllClients();
-
-    if (clients.empty()) {
-        throw *new exception;
-    }
-
-    cout << "All clients:" << endl;
-    printAllClients();
-    int clientNo;
-    do {
-        cout << "Choose a client (" << 1 << "-" << clients.size() << "): ";
-
-        string inputClientNo;
-        cin >> inputClientNo;
-
-        try {
-            clientNo = stoi(inputClientNo);
-        } catch (invalid_argument& invalidArgument) {
-            clientNo = -1;
-            cout << "Write a number!" << endl;
-        } catch (out_of_range& outOfRange) {
-            clientNo = -1;
-            cout << "Write a smaller number!" << endl;
-        }
-        clientNo -= 1; // -1 => because in printing clients using index+1
-    } while (clientNo < 0 || clientNo > clients.size());
-
-    client = *clients.at(clientNo);
-
-    cout << endl << "Write the products: " << endl;
-
-    readAndSaveProduct(products);
-
-    do {
-        cout << endl << "Do you want to add one more? (Y/N): ";
-
-        string program;
-        cin >> program;
-        Utils::toLowerCase(&program);
-
-        if (program == yesChar) {
-            cout << endl;
-            readAndSaveProduct(products);
-        } else if (program == noChar) {
-            return;
-        } else {
-            cout << endl << "Wrong input. Try again." << endl;
-        }
-    } while (true);
-}
-
-vector<Client*> getAllClients() {
-    ClientRepository clientRepository(pathToClientsFile);
-    vector<Client*> clients = clientRepository.getAll();
-
-    return clients;
-}
-
-string makeClientLine(const Client& client) {
-    stringstream clientLine;
-    clientLine << client.getFullName() << " <" << client.getEmail() << ">";
-
-    return clientLine.str();
-}
-
-void printAllClients() {
-    vector<Client*> clients = getAllClients();
-
-    vector<string> clientLines;
-    for (int i = 0; i < clients.size(); ++i) {
-        Client* client = clients.at(i);
-
-        string clientLine = makeClientLine(*client);
-
-        stringstream fullClientLine;
-        int clientNo = i + 1;
-        fullClientLine << clientNo << ") " << clientLine;
-
-        clientLines.push_back(fullClientLine.str());
-    }
-
-    for (const string& line: clientLines) {
-        cout << line << endl;
-    }
-}
-
-void readAndSaveProduct(map<string, int>& products) {
-    string name;
-    cout << "Name: ";
-    do {
-        getline(cin, name);
-    } while (name.empty());
-
-    int amount;
-    do {
-        cout << "Amount: ";
-
-        string inputAmount;
-        cin >> inputAmount;
-
-        try {
-            amount = stoi(inputAmount);
-        } catch (invalid_argument& invalidArgument) {
-            amount = -1;
-            cout << "Write a number!" << endl;
-        } catch (out_of_range& outOfRange) {
-            amount = -1;
-            cout << "Write a smaller number!" << endl;
-        }
-    } while (amount < 0);
-
-    products[name] = amount;
-}
-
-void printProducts(map<string, int>& products) {
-    for (pair<string, int> product: products) {
-        int amount = product.second;
-
-        cout << product.first << " - " << amount;
-
-        if (amount == 1)
-            cout << " unit";
-        else
-            cout << " units";
-
-        cout << endl;
-    }
-}
-
-void saveOrder(Order& order) {
-    ClientRepository clientRepository(pathToClientsFile);
-    ProductsRepository productsRepository(pathToProductsFile);
-    OrderRepository orderRepository(pathToOrdersFile, &clientRepository, &productsRepository);
-
-    orderRepository.save(&order);
+    OrderAddingDialog::addOrder();
 }
 
 void OrdersDialog::listAllOrders() {
